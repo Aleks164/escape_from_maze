9b761e0fc253c0b1cbea253df41b9da95da37261
Aleks164 <alekseyleha@mail.ru>
2022-09-22 18:48:42 +0300

    add fix

--- src/calcShortWay.ts
+++ src/calcShortWay.ts
@@ -3,14 +3,14 @@ import { CrossesItemType } from "./index";
 export function calcShortWay(crossesList: CrossesItemType[]) {
   let lastStep = crossesList.pop() as CrossesItemType;
   let lastStepKey = Object.keys(lastStep)[0];
-  let prevStepName = lastStep[lastStepKey]['from'];
-  let prevStepLog = lastStep[lastStepKey]['coord'];
-  let resultLog = [...prevStepLog];
+  let prevStepName = lastStep[lastStepKey].from;
+  let prevStepLog = lastStep[lastStepKey].coord;
+  const resultLog = [...prevStepLog];
   while (prevStepName) {
     lastStep = crossesList.find(item => item[prevStepName!]) as CrossesItemType;
     lastStepKey = Object.keys(lastStep)[0];
-    prevStepName = lastStep[lastStepKey]['from'];
-    prevStepLog = lastStep[lastStepKey]['coord'];
+    prevStepName = lastStep[lastStepKey].from;
+    prevStepLog = lastStep[lastStepKey].coord;
     resultLog.unshift(...prevStepLog);
   }
   return resultLog;
